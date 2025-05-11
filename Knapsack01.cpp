6e7c21dfcf374282d4dc1d0056541271429dfd5a
ratulhn <ratulhossain.bd2003@gmail.com>
2025-05-11 20:21:04 +0600

    0/1Knapsackcpp

--- Knapsack01.cpp
+++ Knapsack01.cpp
@@ -1,27 +1,39 @@
+
+/*
+********************************
+********************************
+
+NAME: MD. RATUL HOSSAIN
+ID: 12308014
+CodeForces: https://codeforces.com/profile/Phantom_R
+
+********************************
+********************************
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-
-
-
 int main() {
+    int n, wt;
+    cin >> n >> wt;
+    vector<int> val(n), w(n);
+    for (int i = 0; i < n; i++) cin >> val[i];
+    for (int i = 0; i < n; i++) cin >> w[i];
 
-    int n,wt; cin>>n>>wt;
-    vector<int>val(n),w(n);
-    for(int i=0; i<n; i++) cin>>val[i];
-    for(int i=0; i<n; i++) cin>>w[i];
+    vector<vector<int>> dp(n + 1, vector<int>(wt + 1, 0));
 
-    int dp[n+1][n+1];
-    memset(dp,0,sizeof(dp));
-
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=n; j++) {
-            if(w[i-1]<=j) dp[i][j]=max(dp[i-1][j],val[i-1]+dp[i-1][j-w[i-1]]);
-            else dp[i][j]=dp[i-1][j];
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= wt; j++) {
+            if (w[i - 1] <= j)
+                dp[i][j] = max(dp[i - 1][j], val[i - 1] + dp[i - 1][j - w[i - 1]]);
+            else
+                dp[i][j] = dp[i - 1][j];
         }
     }
 
-    cout << dp[n][n] << "\n";
+    cout << dp[n][wt] << "\n";
 
     return 0;
 }
